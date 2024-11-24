84d1507b8f1a9b05aa51a42cd76e5bd40ccaf64e
Andreas Madsack <andreas@madflex.de>
2024-11-24 17:40:34 +0100

    [waste] add current path of run.sh to run.py call

--- str-waste/run.sh
+++ str-waste/run.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 
-python run.py "https://service.stuttgart.de/lhs-services/aws/api/ical?street=Rathausplatz&streetnr=1"
+python $(dirname -- "${BASH_SOURCE[0]}")/run.py "https://service.stuttgart.de/lhs-services/aws/api/ical?street=Rathausplatz&streetnr=1"
